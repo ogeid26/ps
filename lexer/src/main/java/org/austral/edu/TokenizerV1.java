398d67a03ec08d5dc2f83be2492ebc8675989dbd
ignam <ignacio.mendez@ing.austral.edu.ar>
2023-07-18 14:13:47 -0300

    corrección formatter nacho

--- lexer/src/main/java/org/austral/edu/TokenizerV1.java
+++ lexer/src/main/java/org/austral/edu/TokenizerV1.java
@@ -8,59 +8,25 @@ public class TokenizerV1 implements Tokenizer {
 
     @Override
     public Token tokenize(String currentString, int from, int fromCol, int col, int row) {
-        Token result = null;
-        TokenTypeV1 resultType = null;
         for (TokenTypeV1 type: TokenTypeV1.values()) {
             if (type.getName().equals("")) {
                 continue;
             }
             if (type.getName().equals(currentString)){
-                result = new Token(type, currentString);
-                resultType = type;
-                if (type == TokenTypeV1.PRINTLN){
-                    active = true;
-                }
-                break;
+                return new Token(type, currentString);
             }
             if (currentString.matches("^[0-9]+.?[0-9]*")) {
-                result = new Token(TokenTypeV1.NUMBER, currentString);
-                resultType = TokenTypeV1.NUMBER;
-                break;
+                return new Token(TokenTypeV1.NUMBER, currentString);
             }
             if (currentString.contains("\"") || currentString.contains("'")) {
-                result = new Token(TokenTypeV1.STRING, currentString);
-                resultType = TokenTypeV1.STRING;
-                break;
+                return new Token(TokenTypeV1.STRING, currentString);
             }
         }
-        if (result == null){
-            if (checkCase(currentString)) {
-                result = new Token(TokenTypeV1.IDENTIFIER, currentString);
-                resultType = TokenTypeV1.IDENTIFIER;
-            } else {
-                throw new RuntimeException("Case Error");
-            }
+        if (checkCase(currentString)) {
+            return new Token(TokenTypeV1.IDENTIFIER, currentString);
+        }else{
+            throw new RuntimeException("Case Error");
         }
-        if (active){
-            tokens.add(resultType);
-            if (tokens.size() == 4){
-                active = false;
-                if (tokens.get(1) == TokenTypeV1.L_PAR){
-                    if (tokens.get(2) == TokenTypeV1.IDENTIFIER || tokens.get(2) == TokenTypeV1.NUMBER || tokens.get(2) == TokenTypeV1.STRING){
-                        if (tokens.get(3) == TokenTypeV1.R_PAR){
-                            tokens = new ArrayList<>();
-                        }else{
-                            throw new RuntimeException("Error in function");
-                        }
-                    }else{
-                        throw new RuntimeException("Error in function");
-                    }
-                }else{
-                    throw new RuntimeException("Error in function");
-                }
-            }
-        }
-        return result;
     }
 
     private boolean checkCase(String word) {
--- lexer/src/main/java/org/austral/edu/TokenizerV2.java
+++ lexer/src/main/java/org/austral/edu/TokenizerV2.java
@@ -8,47 +8,19 @@ public class TokenizerV2 extends TokenizerV1 {
     public Token tokenize(String currentString, int from, int fromCol, int col, int row) {
         Token token = super.tokenize(currentString, from, fromCol, col, row);
         if (token.tokenType.equals(TokenTypeV1.IDENTIFIER)) {
-            TokenTypeV2 resultType = null;
             for (TokenTypeV2 type: TokenTypeV2.values()) {
                 if (type.getName().equals("")) {
                     continue;
                 }
                 if (type.getName().equals(currentString)){
                     token = new Token(type, currentString);
-                    resultType = type;
-                    if (type == TokenTypeV2.READ_INPUT){
-                        active = true;
-                    }
                     break;
                 }
                 if (currentString.equals("true") || currentString.equals("false")) {
                     token = new Token(TokenTypeV2.BOOLEAN, currentString);
-                    resultType = type;
                     break;
                 }
             }
-            if (active){
-                if (resultType != null) {
-                    tokens.add(resultType);
-                }
-                if (tokens.size() == 4){
-                    active = false;
-                    if (tokens.get(1) == TokenTypeV1.L_PAR){
-                        if (tokens.get(2) == TokenTypeV1.IDENTIFIER || tokens.get(2) == TokenTypeV1.NUMBER || tokens.get(2) == TokenTypeV1.STRING || tokens.get(2) == TokenTypeV2.BOOLEAN){
-                            if (tokens.get(3) == TokenTypeV1.R_PAR){
-                                tokens = new ArrayList<>();
-                            }else{
-                                throw new RuntimeException("Error in function");
-                            }
-                        }else{
-                            throw new RuntimeException("Error in function");
-                        }
-                    }else{
-                        throw new RuntimeException("Error in function");
-                    }
-                }
-            }
-            return token;
         }
         return token;
     }
