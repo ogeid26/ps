b6fc29d21e68f84a8a5c8a208ac635a744ab8ed5
ignam <ignacio.mendez@ing.austral.edu.ar>
2023-07-18 23:21:47 -0300

    primera tanda tests

--- lexer/src/test/java/org/austral/edu/LexerTest.java
+++ lexer/src/test/java/org/austral/edu/LexerTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -17,10 +19,250 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class LexerTest {
 
     @Test
-    public void test001_lexer() throws UnclosedStringLiteralException, UnclosedParenthesesException, UnclosedBracesException {
-        InputProvider string = new StringInput("let name: string = \"Miguel\" 8 8.9 32132 'Hello'+-*/ ");
+    public void test001_Declare_Assign() throws UnclosedStringLiteralException, UnclosedParenthesesException, UnclosedBracesException {
+        InputProvider string1 = new StringInput("let firstName: string = \"Miguel\"; ");
         Lexer lexer = new LexerV1();
-        List<Token> tokens =  lexer.lex(string);
+        List<Token> tokens =  lexer.lex(string1);
+
+        List<TokenType> types = new ArrayList<>();
+        List<String> values = new ArrayList<>();
+
+        for (Token token: tokens) {
+            types.add(token.tokenType);
+            values.add(token.content);
+        }
+
+        List<TokenType> result1 = Arrays.asList(TokenTypeV1.LET, TokenTypeV1.IDENTIFIER, TokenTypeV1.DECLARATION,
+                TokenTypeV1.STRING_TYPE, TokenTypeV1.ASSIGNATION, TokenTypeV1.STRING, TokenTypeV1.SEMICOLON);
+
+        List<String> result2 = Arrays.asList("let", "firstName", ":", "string", "=", "Miguel", ";");
+
+        assertEquals(result1, types);
+
+        assertEquals(result2, values);
+
+        for (Token token: tokens) {
+            System.out.println(token.tokenType + " --> " + token.content);
+        }
+
+    }
+
+    @Test
+    public void test002_Declare_then_Assign() throws UnclosedStringLiteralException, UnclosedParenthesesException, UnclosedBracesException {
+        InputProvider string1 = new StringInput("let x: number; " +
+                "x = 1 + 2;");
+        Lexer lexer = new LexerV1();
+        List<Token> tokens =  lexer.lex(string1);
+
+        List<TokenType> types = new ArrayList<>();
+        List<String> values = new ArrayList<>();
+
+        for (Token token: tokens) {
+            types.add(token.tokenType);
+            values.add(token.content);
+        }
+
+        List<TokenType> result1 = Arrays.asList(TokenTypeV1.LET, TokenTypeV1.IDENTIFIER, TokenTypeV1.DECLARATION,
+                TokenTypeV1.NUMBER_TYPE, TokenTypeV1.SEMICOLON, TokenTypeV1.IDENTIFIER, TokenTypeV1.ASSIGNATION,
+                TokenTypeV1.NUMBER, TokenTypeV1.PLUS, TokenTypeV1.NUMBER, TokenTypeV1.SEMICOLON);
+
+        List<String> result2 = Arrays.asList("let", "x", ":", "number", ";", "x", "=", "1", "+", "2", ";");
+
+        assertEquals(result1, types);
+
+        assertEquals(result2, values);
+
+        for (Token token: tokens) {
+            System.out.println(token.tokenType + " --> " + token.content);
+        }
+
+    }
+
+    @Test
+    public void test003_Assign_Variable() throws UnclosedStringLiteralException, UnclosedParenthesesException, UnclosedBracesException {
+        InputProvider string1 = new StringInput("let first_name: string = \"Miguel\"; " +
+                "let y: string;" +
+                "y = first_name;");
+        Lexer lexer = new LexerV1();
+        List<Token> tokens =  lexer.lex(string1);
+
+        List<TokenType> types = new ArrayList<>();
+        List<String> values = new ArrayList<>();
+
+        for (Token token: tokens) {
+            types.add(token.tokenType);
+            values.add(token.content);
+        }
+
+        List<TokenType> result1 = Arrays.asList(TokenTypeV1.LET, TokenTypeV1.IDENTIFIER, TokenTypeV1.DECLARATION,
+                TokenTypeV1.STRING_TYPE, TokenTypeV1.ASSIGNATION, TokenTypeV1.STRING, TokenTypeV1.SEMICOLON,
+                TokenTypeV1.LET, TokenTypeV1.IDENTIFIER, TokenTypeV1.DECLARATION, TokenTypeV1.STRING_TYPE,
+                TokenTypeV1.SEMICOLON, TokenTypeV1.IDENTIFIER, TokenTypeV1.ASSIGNATION, TokenTypeV1.IDENTIFIER,
+                TokenTypeV1.SEMICOLON);
+
+        List<String> result2 = Arrays.asList("let", "first_name", ":", "string", "=", "Miguel", ";","let", "y", ":",
+                "string", ";", "y", "=", "first_name", ";");
+
+        assertEquals(result1, types);
+
+        assertEquals(result2, values);
+
+        for (Token token: tokens) {
+            System.out.println(token.tokenType + " --> " + token.content);
+        }
+
+    }
+
+    @Test
+    public void test004_Assign_Declare_Variable() throws UnclosedStringLiteralException, UnclosedParenthesesException, UnclosedBracesException {
+        InputProvider string1 = new StringInput("let x: number = 100; " +
+                "let y: number = x;");
+        Lexer lexer = new LexerV1();
+        List<Token> tokens =  lexer.lex(string1);
+
+        List<TokenType> types = new ArrayList<>();
+        List<String> values = new ArrayList<>();
+
+        for (Token token: tokens) {
+            types.add(token.tokenType);
+            values.add(token.content);
+        }
+
+        List<TokenType> result1 = Arrays.asList(TokenTypeV1.LET, TokenTypeV1.IDENTIFIER, TokenTypeV1.DECLARATION,
+                TokenTypeV1.NUMBER_TYPE, TokenTypeV1.ASSIGNATION, TokenTypeV1.NUMBER, TokenTypeV1.SEMICOLON,
+                TokenTypeV1.LET, TokenTypeV1.IDENTIFIER, TokenTypeV1.DECLARATION, TokenTypeV1.NUMBER_TYPE,
+                 TokenTypeV1.ASSIGNATION, TokenTypeV1.IDENTIFIER, TokenTypeV1.SEMICOLON);
+
+        List<String> result2 = Arrays.asList("let", "x", ":", "number", "=", "100", ";","let", "y", ":",
+                "number", "=", "x", ";");
+
+        assertEquals(result1, types);
+
+        assertEquals(result2, values);
+
+        for (Token token: tokens) {
+            System.out.println(token.tokenType + " --> " + token.content);
+        }
+
+    }
+
+    @Test
+    public void test005_Println() throws UnclosedStringLiteralException, UnclosedParenthesesException, UnclosedBracesException {
+        InputProvider string1 = new StringInput("println(6/3-1*2);");
+        Lexer lexer = new LexerV2();
+        List<Token> tokens =  lexer.lex(string1);
+
+        List<TokenType> types = new ArrayList<>();
+        List<String> values = new ArrayList<>();
+
+        for (Token token: tokens) {
+            types.add(token.tokenType);
+            values.add(token.content);
+        }
+
+        List<TokenType> result1 = Arrays.asList(TokenTypeV1.PRINTLN, TokenTypeV1.L_PAR, TokenTypeV1.NUMBER,
+                TokenTypeV1.DIVIDE, TokenTypeV1.NUMBER, TokenTypeV1.MINUS, TokenTypeV1.NUMBER,
+                TokenTypeV1.MULTIPLY, TokenTypeV1.NUMBER, TokenTypeV1.R_PAR, TokenTypeV1.SEMICOLON);
+
+        List<String> result2 = Arrays.asList("println", "(", "6", "/", "3", "-", "1","*", "2", ")", ";");
+
+        assertEquals(result1, types);
+
+        assertEquals(result2, values);
+
+        for (Token token: tokens) {
+            System.out.println(token.tokenType + " --> " + token.content);
+        }
+
+    }
+    @Test
+    public void test006_Boolean() throws UnclosedStringLiteralException, UnclosedParenthesesException, UnclosedBracesException {
+        InputProvider string1 = new StringInput("let x:boolean = true;");
+        Lexer lexer = new LexerV2();
+        List<Token> tokens =  lexer.lex(string1);
+
+        List<TokenType> types = new ArrayList<>();
+        List<String> values = new ArrayList<>();
+
+        for (Token token: tokens) {
+            types.add(token.tokenType);
+            values.add(token.content);
+        }
+
+        List<TokenType> result1 = Arrays.asList(TokenTypeV1.LET, TokenTypeV1.IDENTIFIER, TokenTypeV1.DECLARATION,
+                TokenTypeV2.BOOLEAN_TYPE, TokenTypeV1.ASSIGNATION, TokenTypeV2.BOOLEAN, TokenTypeV1.SEMICOLON);
+
+        List<String> result2 = Arrays.asList("let", "x", ":", "boolean", "=", "true", ";");
+
+        assertEquals(result1, types);
+
+        assertEquals(result2, values);
+
+        for (Token token: tokens) {
+            System.out.println(token.tokenType + " --> " + token.content);
+        }
+
+    }
+
+    @Test
+    public void test007_If() throws UnclosedStringLiteralException, UnclosedParenthesesException, UnclosedBracesException {
+        InputProvider string1 = new StringInput("if(true){" +
+                "x;" +
+                "}else{" +
+                "y;" +
+                "}");
+        Lexer lexer = new LexerV2();
+        List<Token> tokens =  lexer.lex(string1);
+
+        List<TokenType> types = new ArrayList<>();
+        List<String> values = new ArrayList<>();
+
+        for (Token token: tokens) {
+            types.add(token.tokenType);
+            values.add(token.content);
+        }
+
+        List<TokenType> result1 = Arrays.asList(TokenTypeV2.IF, TokenTypeV1.L_PAR, TokenTypeV2.BOOLEAN,
+                TokenTypeV1.R_PAR, TokenTypeV2.L_BRACES, TokenTypeV1.IDENTIFIER, TokenTypeV1.SEMICOLON,
+                TokenTypeV2.R_BRACES, TokenTypeV2.ELSE, TokenTypeV2.L_BRACES, TokenTypeV1.IDENTIFIER,
+                TokenTypeV1.SEMICOLON, TokenTypeV2.R_BRACES);
+
+        List<String> result2 = Arrays.asList("if", "(", "true", ")", "{", "x", ";" ,"}", "else", "{", "y", ";","}");
+
+        assertEquals(result1, types);
+
+        assertEquals(result2, values);
+
+        for (Token token: tokens) {
+            System.out.println(token.tokenType + " --> " + token.content);
+        }
+
+    }
+
+    @Test
+    public void test008_ReadInput() throws UnclosedStringLiteralException, UnclosedParenthesesException, UnclosedBracesException {
+        InputProvider string1 = new StringInput("const x:string = readInput(\"Enter your Name\");");
+        Lexer lexer = new LexerV2();
+        List<Token> tokens =  lexer.lex(string1);
+
+        List<TokenType> types = new ArrayList<>();
+        List<String> values = new ArrayList<>();
+
+        for (Token token: tokens) {
+            types.add(token.tokenType);
+            values.add(token.content);
+        }
+
+        List<TokenType> result1 = Arrays.asList(TokenTypeV2.CONST, TokenTypeV1.IDENTIFIER, TokenTypeV1.DECLARATION,
+                TokenTypeV1.STRING_TYPE, TokenTypeV1.ASSIGNATION, TokenTypeV2.READ_INPUT, TokenTypeV1.L_PAR,
+                TokenTypeV1.STRING, TokenTypeV1.R_PAR, TokenTypeV1.SEMICOLON);
+
+        List<String> result2 = Arrays.asList("const", "x", ":", "string", "=", "readInput", "(", "Enter your Name",
+                ")", ";");
+
+        assertEquals(result1, types);
+
+        assertEquals(result2, values);
 
         for (Token token: tokens) {
             System.out.println(token.tokenType + " --> " + token.content);
@@ -79,17 +321,6 @@ public class LexerTest {
         Lexer lexer = new LexerV1();
         UnclosedParenthesesException exception = Assertions.assertThrows(UnclosedParenthesesException.class, () -> lexer.lex(new StringInput("println('Hi';")));
         System.out.println(exception.getMessage());
-    }
-
-    @Test
-    public void sandbox() throws UnclosedBracesException, UnclosedParenthesesException, UnclosedStringLiteralException {
-
-        Lexer lexer = new LexerV2();
-        List<Token> tokens = lexer.lex(new StringInput(" let x:string = readInput(\"Miguel\");"));
-        for (Token token: tokens) {
-            System.out.println(token.tokenType + " -> " + token.content);
-        }
-        assertEquals(3,3);
     }
 }
 
--- parser/src/test/java/parser/ParserTest.java
+++ parser/src/test/java/parser/ParserTest.java
@@ -1,19 +1,19 @@
 package parser;
 
-import ast.AbstractSyntaxTree;
-import ast.AssignDeclareNode;
-import ast.LiteralNode;
-import ast.Node;
+import ast.*;
 import exceptions.*;
 import org.austral.edu.*;
+import org.austral.edu.Nodes.IfNode;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ParserTest {
 
@@ -28,6 +28,103 @@ public class ParserTest {
         parserV1 = new ParserV1();
         lexerV2 = new LexerV2();
         parserV2 = new ParserV2();
+    }
+
+    @Test
+    public void test001_Declare_Assign() throws UnclosedStringLiteralException, UnclosedParenthesesException, UnclosedBracesException, UnexpectedTokenException, ExpectedTokenException {
+        InputProvider string1 = new StringInput("let name: string = \"Miguel\"; ");
+        List<Token> tokens = lexerV1.lex(string1);
+        AbstractSyntaxTree ast = parserV1.parse(tokens);
+        printNode(ast);
+
+    }
+
+    @Test
+    public void test002_Declare_then_Assign() throws UnclosedStringLiteralException, UnclosedParenthesesException, UnclosedBracesException, UnexpectedTokenException, ExpectedTokenException {
+        InputProvider string1 = new StringInput("let x: number; " +
+                "x = 1 + 2;");
+        List<Token> tokens = lexerV1.lex(string1);
+        AbstractSyntaxTree ast = parserV1.parse(tokens);
+        printNode(ast);
+
+    }
+
+    @Test
+    public void test003_Assign_Variable() throws UnclosedStringLiteralException, UnclosedParenthesesException, UnclosedBracesException, UnexpectedTokenException, ExpectedTokenException {
+        InputProvider string1 = new StringInput("let name: string = \"Miguel\"; " +
+                "let y: string;" +
+                "y = name;");
+        List<Token> tokens = lexerV1.lex(string1);
+        AbstractSyntaxTree ast = parserV1.parse(tokens);
+        printNode(ast);
+
+    }
+
+    @Test
+    public void test004_Assign_Declare_Variable() throws UnclosedStringLiteralException, UnclosedParenthesesException, UnclosedBracesException, UnexpectedTokenException, ExpectedTokenException {
+        InputProvider string1 = new StringInput("let x: number = 100; " +
+                "let y: number = x;");
+        List<Token> tokens = lexerV1.lex(string1);
+        AbstractSyntaxTree ast = parserV1.parse(tokens);
+        printNode(ast);
+
+    }
+
+    @Test
+    public void test005_Println() throws UnclosedStringLiteralException, UnclosedParenthesesException, UnclosedBracesException, UnexpectedTokenException, ExpectedTokenException {
+        InputProvider string1 = new StringInput("let x:string = 2;" +
+                "println(6/3-1*x);");
+        List<Token> tokens = lexerV2.lex(string1);
+        AbstractSyntaxTree ast = parserV2.parse(tokens);
+
+        assertTrue(ast.children.get(1) instanceof PrintNode);
+
+        assertTrue(ast.children.get(1).getChildren().get(0) instanceof SubtractionNode);
+
+        assertTrue(((SubtractionNode) ast.children.get(1).getChildren().get(0)).getLeft() instanceof DivisionNode);
+
+        assertTrue(((SubtractionNode) ast.children.get(1).getChildren().get(0)).getLeft().getLeft() instanceof LiteralNode);
+        assertTrue(((SubtractionNode) ast.children.get(1).getChildren().get(0)).getLeft().getRight() instanceof LiteralNode);
+
+
+        assertTrue(((SubtractionNode) ast.children.get(1).getChildren().get(0)).getRight() instanceof MultiplicationNode);
+
+        assertTrue(((SubtractionNode) ast.children.get(1).getChildren().get(0)).getLeft().getLeft() instanceof LiteralNode);
+        assertTrue(((SubtractionNode) ast.children.get(1).getChildren().get(0)).getLeft().getRight() instanceof LiteralNode);
+
+        printNode(ast);
+
+    }
+    @Test
+    public void test006_Boolean() throws UnclosedStringLiteralException, UnclosedParenthesesException, UnclosedBracesException, UnexpectedTokenException, ExpectedTokenException {
+        InputProvider string1 = new StringInput("let x:boolean = true;");
+        List<Token> tokens = lexerV2.lex(string1);
+        AbstractSyntaxTree ast = parserV2.parse(tokens);
+        printNode(ast);
+    }
+
+    @Test
+    public void test007_If() throws UnclosedStringLiteralException, UnclosedParenthesesException, UnclosedBracesException, UnexpectedTokenException, ExpectedTokenException {
+        InputProvider string1 = new StringInput("if(true){" +
+                "println(\"x\");" +
+                "}else{" +
+                "println(\"y\");" +
+                "}");
+        List<Token> tokens = lexerV2.lex(string1);
+        AbstractSyntaxTree ast = parserV2.parse(tokens);
+
+        assertTrue(ast.children.get(0) instanceof IfNode);
+
+        printNode(ast);
+    }
+
+    @Test
+    public void test008_ReadInput() throws UnclosedStringLiteralException, UnclosedParenthesesException, UnclosedBracesException, UnexpectedTokenException, ExpectedTokenException {
+        InputProvider string1 = new StringInput("const x:string = readInput(\"Enter your Name\");");
+        List<Token> tokens = lexerV2.lex(string1);
+        AbstractSyntaxTree ast = parserV2.parse(tokens);
+        printNode(ast);
+
     }
 
     @Test
